220aa7289de9eff3dd0c6ab757a0fc1c573e008b
Kenjiro Aguilera <kenjiro.mendez@gmail.com>
2025-12-03 22:36:53 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARGareTpBEa1GrfI4og9C5f7I4ab-9buQ",
-    authDomain: "invernadero-a0e85.firebaseapp.com",
-    databaseURL: "https://invernadero-a0e85-default-rtdb.firebaseio.com", 
-    projectId: "invernadero-a0e85",
-    storageBucket: "invernadero-a0e85.firebasestorage.app",
-    messagingSenderId: "2152191338",
-    appId: "1:2152191338:web:1433e0c243bc04b9870113",
-    measurementId: "G-6EF5CSRL3B"
+    apiKey: "AIzaSyDzaqbPwY1_DFDfrI80aKesCtevsLkxjIU",
+    authDomain: "invernadero-v2.firebaseapp.com",
+    databaseURL: "https://invernadero-v2-default-rtdb.firebaseio.com", 
+    projectId: "invernadero-v2",
+    storageBucket: "invernadero-v2.firebasestorage.app",
+    messagingSenderId: "200557096758",
+    appId: "1:200557096758:web:f6b1e94ff436a9ca394d97",
+    measurementId: "G-D9Z3L6PVZC"
 };
 
 // Inicializar Firebase
